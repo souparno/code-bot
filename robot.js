c372bd2f73c468193a432827d2f53e51e017a598
souparno <souparno.majumder@gmail.com>
2021-05-22 01:38:41 +0530

    parsing the string with a blank

---
+++ robot.js
@@ -0,0 +1,168 @@
+const fs = require('fs');
+
+
+// Simple JavaScript Templating
+// John Resig - https://johnresig.com/ - MIT Licensed
+(function() {
+
+    this.tmpl = function tmpl(str, data) {
+        // Generate a reusable function that will serve as a template
+        // generator (and which will be cached).
+        var fn = new Function("obj",
+            "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+            // Introduce the data as local variables using with(){}
+            "with(obj){p.push('" +
+
+            // Convert the template into pure JavaScript
+            str
+            .replace(/[\r\t\n]/g, " ")
+            .split("<%").join("\t")
+            .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+            .replace(/\t=(.*?)%>/g, "',$1,'")
+            .split("\t").join("');")
+            .split("%>").join("p.push('")
+            .split("\r").join("\\'") +
+            "');}return p.join('');");
+
+        // Provide some basic currying to the user
+        return data ? fn(data) : fn;
+    };
+})();
+
+
+transformString = (str) => {
+    return str
+        .replace(/([\.\,\;\(\)\[\]\{\}\<\>\=\+])/g, ` $1 `)
+        .replace(/(\s)*/g, `$1`)
+        .replace(/\n/g, ``)
+}
+
+
+class Extract {
+    constructor() {
+        this.arr = [];
+    }
+
+    get() {
+        return this.arr;
+    }
+
+    shift() {
+        return this.arr.shift();
+    }
+
+    set(regex, str) {
+        let m;
+        while ((m = regex.exec(str)) !== null) {
+            // This is necessary to avoid infinite loops with zero-width matches
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+
+            // The result can be accessed through the `m`-variable.
+            m.forEach((match, groupIndex) => {
+                if (groupIndex) this.arr.push(match)
+            });
+        }
+
+        return str;
+    }
+}
+
+let extract = new Extract();
+
+parse = (str) => {
+    str = transformString(str)
+        .split("(").join("\\(")
+        .split(")").join("\\)")
+        .split("[").join("\\[")
+        .split("]").join("\\]")
+        .split(".").join("\\.")
+
+    const regex = /<\s%\s=\sargs\s\\\[\s([0-9]*)\s\\]\s%\s>/g;;
+    const subst = `(.*)`;
+    return "^" + extract.set(regex, str).replace(regex, subst) + "$";
+}
+
+
+class Instruction {
+    constructor(str) {
+        this.instructions = {};
+        this.str = transformString(str);
+        this.regex = "";
+    }
+
+    if (regex) {
+        this.regex = parse(regex);
+
+        return this;
+    }
+
+    then(prompt) {
+        this.instructions[this.regex] = prompt;
+
+        return this;
+    }
+
+    prompt() {
+        for (var regex in this.instructions) {
+            var p = this.run(new RegExp(regex), this.instructions[regex])
+            if (p) return p;
+        }
+
+        return null;
+    }
+
+    run(regex, prompt) {
+        let m;
+        let args = new Array(extract.get().length);
+        if ((m = regex.exec(this.str)) !== null) {
+            m.forEach((match, groupIndex) => {
+                if (groupIndex) args[extract.shift()] = match;
+            });
+
+            return tmpl(prompt, {
+                args: args
+            });
+        }
+    }
+}
+
+
+let instruction = new Instruction(fs.readFileSync('./palindrom.java', 'utf8'));
+
+
+instruction.if(`
+
+import <%=args[0]%>;
+
+class <%=args[1]%>{
+
+  public static void main(<%=args[2]%>) {
+
+    <%=args[11]%> a = <%=args[7]%>(<%=args[3]%>);
+
+    <%=args[10]%>(<%=args[4]%>;<%=args[5]%>;<%=args[6]%>){
+      <%=args[8]%>(<%=args[9]%>);
+    }
+  }
+
+}
+`).then(`
+
+ console.log("<%=args[0]%>");
+ console.log("<%=args[1]%>");
+ console.log("<%=args[2]%>");
+ console.log("<%=args[3]%>");
+ console.log("<%=args[4]%>");
+ console.log("<%=args[5]%>");
+ console.log("<%=args[6]%>");
+ console.log("<%=args[7]%>");
+ console.log("<%=args[8]%>");
+ console.log("<%=args[9]%>");
+ console.log("<%=args[10]%>");
+ console.log("<%=args[11]%>");
+`);
+
+eval(instruction.prompt());
